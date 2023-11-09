90b110d7eec061459eaff3d7b4087d73d62ec009
Eduardo O. Santos <75816516+Eduardo-O-Santos@users.noreply.github.com>
2023-11-09 11:06:38 -0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <div className='app'>
+        <h1>Boilerplate Vite + React</h1>
+      </div>
+    </>
+  );
+}
+
+export { App };
